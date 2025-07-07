09f145a472179a7a0fb60e0b8fc5a4cd0e2f5d82
Akshay P <147929471+Akshayp0105@users.noreply.github.com>
2025-07-07 21:47:38 +0530

    Add files via upload

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Contact() {
+  return (
+    <div style={{ padding: "2rem" }}>
+      <h1>Contact Us</h1>
+      <p>You can reach us at support@korvet.in or visit our office.</p>
+    </div>
+  );
+}
+
+export default Contact;
